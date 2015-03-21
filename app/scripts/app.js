1ee4c62e99a8f3acb9ce0ea9af31502e565d5e12
Justin Gardner <justin@justingardner.com>
2015-03-21 17:00:54 -0400

    added backbone router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 var View = require('./views/main');
 
-view = new View({el: '#content'});
+view = new View();
 
 console.log('WHOA app.js loaded!');
--- app/scripts/views/main.js
+++ app/scripts/views/main.js
@@ -2,25 +2,37 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
+var _ = require('lodash');
+
 var key = require('keymaster');
 
-var _defaultView = require('../views/_default');
-var _defaultModel = require('../models/_default');
+var _defaultRouter = require('../router');
 
 module.exports = Backbone.View.extend({
 	el: "#content",
 
-	template: require('../templates/main.ejs'),
+	template: require('../templates/main'),
 
     initialize: function() {
         this.render();
+
+        this.Router = new _defaultRouter({ app: this });
+
+        //pass view to this from router to render different content areas
+        this.listenTo(this.Router, 'router:showView', _.bind(function (view) {
+            this.swapMain(view);
+        }, this));
+
+        Backbone.history.start();
 
         key.setScope('app');
     },
 
     render: function() {
         this.$el.html(this.template());
+    },
 
-        this._defaultView = new _defaultView({ model: new _defaultModel() }).render();
+    swapMain: function (view) {
+        view.render();
     }
 });
